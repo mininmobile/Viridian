44542269275fbd46cb77bd330cbb49391eacc34a
MininMobile <notminin@pm.me>
2019-01-02 18:05:42 +0000

    add canvas utility class

--- viridian/MainWindow.xaml.cs
+++ viridian/MainWindow.xaml.cs
@@ -14,12 +14,41 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace viridian {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window {
+		public UtilCanvas c;
+
 		public MainWindow() {
 			InitializeComponent();
+			c = new UtilCanvas(DisplayCanvas);
+
+			c.DrawText("despacito");
+		}
+	}
+
+	public class UtilCanvas {
+		Canvas target;
+	
+		public UtilCanvas(Canvas canvas) {
+			target = canvas;
+		}
+
+		public void DrawText(string text, double x = 0, double y = 0, double size = 16, Color color = default(Color)) {
+			color = color == default(Color) ? Color.FromRgb(0, 0, 0) : color;
+
+			TextBlock textBlock = new TextBlock();
+
+			// set content
+			textBlock.Text = text;
+
+			// set appearance
+			textBlock.FontSize = size;
+			textBlock.Foreground = new SolidColorBrush(color);
+
+			// set position
+			Canvas.SetLeft(textBlock, x);
+			Canvas.SetTop(textBlock, y);
+
+			target.Children.Add(textBlock);
 		}
 	}
 }
